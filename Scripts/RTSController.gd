e19991f496502e4d98774f787a7f21b2fe1238e0
tiger <thisisstreamliner@gmail.com>
2024-04-13 17:13:38 +0300

    feat: add assets. setup camera movement

---
+++ Scripts/RTSController.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+const MOVE_MARGIN : int = 20
+const MOVE_SPEED : int = 15
+@onready var cam: Camera3D = $Camera3D
+var m_pos := Vector2()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	m_pos = get_viewport().get_mouse_position()
+	camera_movement(m_pos, delta)
+	
+func camera_movement(m_pos, delta):
+	var viewport_size: Vector2 = get_viewport().size
+	var origin: Vector3 = global_transform.origin
+	var move_vec := Vector3()
+	if origin.x > -62:
+		if m_pos.x < MOVE_MARGIN:
+			move_vec.x -= 1
+	if origin.z > -65:
+		if m_pos.y < MOVE_MARGIN:
+			move_vec.z -= 1
+	if origin.x < 62:
+		if m_pos.x > viewport_size.x - MOVE_MARGIN:
+			move_vec.x += 1
+	if origin.z < 90:
+		if m_pos.y > viewport_size.y - MOVE_MARGIN:
+			move_vec.z += 1
+	move_vec = move_vec.rotated(Vector3(0, 1, 0), rad_to_deg(rotation.y))
+	global_translate(move_vec * delta * MOVE_SPEED)
